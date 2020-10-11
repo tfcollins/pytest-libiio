47baa8b31c193ce35addb6ac38f8677b443ebf99
Travis F. Collins <travis.collins@analog.com>
2020-10-11 13:10:07 -0700

    Add mock testing for libiio APIs
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,49 @@
+import pytest
+
 pytest_plugins = "pytester"
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--disable_mock", action="store_true", help="Disable mocking",
+    )
+    parser.addoption(
+        "--hw",
+        action="store",
+        dest="hw_map",
+        default=None,
+        help="Set expected hardware",
+    )
+    parser.addoption(
+        "--test-uri",
+        action="store",
+        dest="uri_val",
+        default=None,
+        help="Set uri to test against",
+    )
+
+
+@pytest.fixture(scope="session")
+def use_mocking(request):
+    if request.config.getoption("--disable_mock"):
+        return False
+    else:
+        return True
+
+
+@pytest.fixture(scope="session")
+def hw_select(request):
+    val = request.config.getoption("--hw")
+    if not val:
+        return "adrv9361"
+    else:
+        return val
+
+
+@pytest.fixture(scope="session")
+def uri_select(request):
+    val = request.config.getoption("--test-uri")
+    if not val:
+        return "ip:192.168.86.56"
+    else:
+        return val
--- tests/test_pytest_libiio.py
+++ tests/test_pytest_libiio.py
@@ -1,15 +1,64 @@
 # -*- coding: utf-8 -*-
+import time
 
+import pytest
+
+sleep = 1
 uri = "ip:192.168.86.35"
+devs = [
+    "ad7291",
+    "ad9361-phy",
+    "xadc",
+    "ad9517-3",
+    "cf-ad9361-dds-core-lpc",
+    "cf-ad9361-lpc",
+]
 
 
-def test_context_fixture(testdir):
+# Mocks
+def mock_Context(uri):
+    class Channel(object):
+        scan_element = True
+
+    class Device(object):
+        channels = [Channel()]
+
+        def __init__(self, name):
+            self.name = name
+
+    class Context(object):
+        def __init__(self, uri, devs):
+            self.attrs = {"uri": uri}
+            self.devices = []
+            for dev in devs:
+                self.devices.append(Device(dev))
+
+    return Context(uri, devs)
+
+
+def mock_scan_contexts():
+    info = uri[2:] + " "
+    uri_s = "ip:" + info
+    info += "(" + ",".join(devs) + ")"
+    ctxs = {uri_s: info}
+
+    return ctxs
+
+
+# Tests
+def test_context_fixture_smoke(testdir, use_mocking, mocker):
     """Make sure that pytest accepts our fixture."""
+    if use_mocking:
+        mocker.patch("iio.scan_contexts", mock_scan_contexts)
+        mocker.patch("iio.Context", mock_Context)
+
+    time.sleep(sleep)
 
     # create a temporary pytest test module
     testdir.makepyfile(
         """
         def test_sth(context_desc):
+            print(context_desc)
             assert isinstance(context_desc,list)
 
     """
@@ -24,8 +73,13 @@ def test_context_fixture(testdir):
     assert result.ret == 0
 
 
-def test_context_fixture_uri(testdir):
+def test_context_fixture_uri_unknown(testdir, use_mocking, uri_select, mocker):
     """Make sure that pytest accepts our fixture."""
+    if use_mocking:
+        mocker.patch("iio.scan_contexts", mock_scan_contexts)
+        mocker.patch("iio.Context", mock_Context)
+
+    time.sleep(sleep)
 
     # create a temporary pytest test module
     testdir.makepyfile(
@@ -41,7 +95,7 @@ def test_context_fixture_uri(testdir):
     )
 
     # run pytest with the following cmd args
-    result = testdir.runpytest("--uri=" + uri, "-v", "-s")
+    result = testdir.runpytest("--uri=" + uri_select, "--scan-verbose", "-v", "-s")
 
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
@@ -50,8 +104,13 @@ def test_context_fixture_uri(testdir):
     assert result.ret == 0
 
 
-def test_context_fixture_uri_adi_map(testdir):
+def test_context_fixture_scan_adi_map(testdir, use_mocking, hw_select, mocker):
     """Make sure that pytest accepts our fixture."""
+    if use_mocking:
+        mocker.patch("iio.scan_contexts", mock_scan_contexts)
+        mocker.patch("iio.Context", mock_Context)
+
+    time.sleep(sleep)
 
     # create a temporary pytest test module
     testdir.makepyfile(
@@ -60,14 +119,16 @@ def test_context_fixture_uri_adi_map(testdir):
             assert context_desc
             found = False
             for ctx in context_desc:
-                if ctx['hw'] == 'adrv9361':
+                if ctx['hw'] == '"""
+        + hw_select
+        + """':
                     found = True
             assert found
     """
     )
 
     # run pytest with the following cmd args
-    result = testdir.runpytest("--adi-hw-map", "--uri=" + uri, "-v", "-s")
+    result = testdir.runpytest("--adi-hw-map", "-v", "-s")
 
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
@@ -76,27 +137,73 @@ def test_context_fixture_uri_adi_map(testdir):
     assert result.ret == 0
 
 
-def test_context_desc_fixture_uri_adi_map(testdir):
+def test_context_fixture_uri_adi_map(
+    testdir, use_mocking, uri_select, hw_select, mocker
+):
     """Make sure that pytest accepts our fixture."""
+    if use_mocking:
+        mocker.patch("iio.scan_contexts", mock_scan_contexts)
+        mocker.patch("iio.Context", mock_Context)
+
+    time.sleep(sleep)
+
+    # create a temporary pytest test module
+    testdir.makepyfile(
+        """
+        def test_sth(context_desc):
+            assert context_desc
+            found = False
+            for ctx in context_desc:
+                if ctx['hw'] == '"""
+        + hw_select
+        + """':
+                    found = True
+            assert found
+    """
+    )
+
+    # run pytest with the following cmd args
+    result = testdir.runpytest("--adi-hw-map", "--uri=" + uri_select, "-v", "-s")
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_context_desc_fixture_uri_adi_map(
+    testdir, use_mocking, uri_select, hw_select, mocker
+):
+    """Make sure that pytest accepts our fixture."""
+    if use_mocking:
+        mocker.patch("iio.scan_contexts", mock_scan_contexts)
+        mocker.patch("iio.Context", mock_Context)
+
+    time.sleep(sleep)
 
     # create a temporary pytest test module
     testdir.makepyfile(
         """
         import pytest
 
-        @pytest.mark.iio_hardware("adrv9361")
+        @pytest.mark.iio_hardware('"""
+        + hw_select
+        + """')
         def test_sth(context_desc):
             assert context_desc
             found = False
             for ctx in context_desc:
-                if ctx['hw'] == 'adrv9361':
+                if ctx['hw'] == '"""
+        + hw_select
+        + """':
                     found = True
             assert found
     """
     )
 
     # run pytest with the following cmd args
-    result = testdir.runpytest("--adi-hw-map", "--uri=" + uri, "-v", "-s")
+    result = testdir.runpytest("--adi-hw-map", "--uri=" + uri_select, "-v", "-s")
 
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
@@ -105,10 +212,10 @@ def test_context_desc_fixture_uri_adi_map(testdir):
     assert result.ret == 0
 
 
-def test_help_message(testdir):
-    result = testdir.runpytest("--help",)
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines(["libiio:", "*--uri=URI*Set libiio URI to utilize"])
+# def test_help_message(testdir):
+#     result = testdir.runpytest("--help",)
+#     # fnmatch_lines does an assertion internally
+#     result.stdout.fnmatch_lines(["libiio:", "*--uri=URI*Set libiio URI to utilize"])
 
 
 # def test_print_scan_message(testdir):
