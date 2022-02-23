8dff7ac2ecd486184dee277ae273e14c8dfe910d
Travis F. Collins <travis.collins@analog.com>
2022-02-22 16:32:00 -0800

    Bump to v0.0.11
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- pytest_libiio/__init__.py
+++ pytest_libiio/__init__.py
@@ -1,3 +1,3 @@
 """A pytest plugin for managing libiio contexts."""
 
-__version__ = "0.0.10"
+__version__ = "0.0.11"
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-libiio",
-    version="0.0.10",
+    version="0.0.11",
     author="Travis F. Collins",
     author_email="travis.collins@analog.com",
     maintainer="Travis F. Collins",
