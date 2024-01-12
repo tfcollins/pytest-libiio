07b40849e65c54dca343a9697197ef6c1941cc5c
Travis F. Collins <travis.collins@analog.com>
2024-01-12 14:09:12 -0700

    [WIP] Add xdist support in emulation mode
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

---
+++ tests/test_xdist_support.py
@@ -0,0 +1,34 @@
+"""Tests for verifying plugin works with pytest-xdist."""
+
+import pytest
+import iio
+import logging
+
+# Create file logger
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.INFO)
+# fh = logging.FileHandler('/tmp/test_xdist_support.log')
+# fh.setLevel(logging.INFO)
+# logger.addHandler(fh)
+
+@pytest.mark.iio_hardware('fmcomms2')
+def test_fmcomms2_xdist(iio_uri):
+    print(f"iio_uri: {iio_uri}")
+    logging.info(f"iio_uri: {iio_uri}")
+
+
+    ctx = iio.Context(iio_uri)
+    for dev in ctx.devices:
+        print(f"dev: {dev.name}, {dev.id}")
+        logging.info(f"dev: {dev.name}, {dev.id}")
+
+
+@pytest.mark.iio_hardware('ad9081')
+def test_ad9081_xdist(iio_uri):
+    print(f"iio_uri: {iio_uri}")
+    logging.info(f"iio_uri: {iio_uri}")
+
+    ctx = iio.Context(iio_uri)
+    for dev in ctx.devices:
+        print(f"dev: {dev.name}, {dev.id}")
+        logging.info(f"dev: {dev.name}, {dev.id}")
