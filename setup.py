e4b710f489ecbf019cb36d72e2688c1864fd0957
Travis F. Collins <travis.collins@analog.com>
2024-09-05 16:38:07 -0600

    Bump version to 0.0.20
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-libiio",
-    version="0.0.19",
+    version="0.0.20",
     author="Travis F. Collins",
     author_email="travis.collins@analog.com",
     maintainer="Travis F. Collins",
