4ef1c661c139f0dd8149e93ee68e7c81ce4f4e75
Travis F. Collins <travis.collins@analog.com>
2026-06-02 16:59:41 -0400

    Add fixes for device property coverage
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- pytest_libiio/mkpatch.py
+++ pytest_libiio/mkpatch.py
@@ -2,7 +2,10 @@ import logging
 from pprint import pprint
 
 import iio
-from iio import ChannelAttr as _Attr
+from iio import ChannelAttr as _CAttr
+from iio import Context as _CtxAttr
+from iio import DeviceAttr as _DAttr
+from iio import _d_get_id, _d_get_name
 
 logger = logging.getLogger(__name__)
 
@@ -21,7 +24,7 @@ def _get_tracker():
 def _read(self):
     """MP Read method to capture attribute reads"""
     attr_name = self.name
-    if self._channel:
+    if hasattr(self, "_channel") and self._channel:
         name_raw = iio._c_get_id(self._channel)
         channel_name = name_raw.decode("ascii") if name_raw is not None else None
         output = iio._c_is_output(self._channel)
@@ -30,7 +33,8 @@ def _read(self):
         device_name = name_raw.decode("ascii") if name_raw is not None else None
     else:
         channel_name = None
-        device_name = self._name
+        name_raw = _d_get_name(self._device)
+        device_name = name_raw.decode("ascii") if name_raw is not None else None
 
     tracker = _get_tracker()
     if channel_name and device_name:
@@ -46,13 +50,18 @@ def _read(self):
     logger.debug(
         f"Reading attribute: {attr_name}, Channel: {channel_name}, Device: {device_name}"
     )
-    return _orig_read(self)
+    if channel_name and device_name:
+        return _orig_chan_read(self)
+    elif device_name:
+        return _orig_dev_read(self)
+    else:
+        raise ValueError("Context properties not supported yet")
 
 
 def _write(self, value):
     """MP Write method to capture attribute writes"""
     attr_name = self.name
-    if self._channel:
+    if hasattr(self, "_channel") and self._channel:
         name_raw = iio._c_get_id(self._channel)
         channel_name = name_raw.decode("ascii") if name_raw is not None else None
         output = iio._c_is_output(self._channel)
@@ -61,7 +70,8 @@ def _write(self, value):
         device_name = name_raw.decode("ascii") if name_raw is not None else None
     else:
         channel_name = None
-        device_name = self._name
+        name_raw = _d_get_name(self._device)
+        device_name = name_raw.decode("ascii") if name_raw is not None else None
 
     tracker = _get_tracker()
     if channel_name and device_name:
@@ -77,15 +87,29 @@ def _write(self, value):
     logger.debug(
         f"Writing attribute: {attr_name}, Channel: {channel_name}, Device: {device_name}, Value: {value}"
     )
-    _orig_write(self, value)
+    if channel_name and device_name:
+        _orig_chan_write(self, value)
+    elif device_name:
+        _orig_dev_write(self, value)
+    else:
+        raise ValueError("Context properties not supported yet")
 
 
-_orig_read = _Attr._read
-_orig_write = _Attr._write
+_orig_chan_read = _CAttr._read
+_orig_chan_write = _CAttr._write
+_orig_dev_read = _DAttr._read
+_orig_dev_write = _DAttr._write
+# _orig_ctx_read = _CtxAttr._read
+# _orig_ctx_write = _CtxAttr._write
+
 # Monkey-patch ChannelAttr to capture reads/writes; setattr is intentional
 # to make the dynamic patching explicit to type checkers.
-setattr(_Attr, "_read", _read)  # noqa: B010
-setattr(_Attr, "_write", _write)  # noqa: B010
+setattr(_CAttr, "_read", _read)  # noqa: B010
+setattr(_CAttr, "_write", _write)  # noqa: B010
+setattr(_DAttr, "_read", _read)  # noqa: B010
+setattr(_DAttr, "_write", _write)  # noqa: B010
+# setattr(_CtxAttr, "_read", _read)  # noqa: B010
+# setattr(_CtxAttr, "_write", _write)  # noqa: B010
 
 
 def set_coverage_tracker(tracker):
@@ -107,8 +131,12 @@ def unset_monkey_patch():
     if coverage_tracker:
         coverage_tracker = None
 
-    setattr(_Attr, "_read", _orig_read)  # noqa: B010
-    setattr(_Attr, "_write", _orig_write)  # noqa: B010
+    setattr(_CAttr, "_read", _orig_chan_read)  # noqa: B010
+    setattr(_CAttr, "_write", _orig_chan_write)  # noqa: B010
+    setattr(_DAttr, "_read", _orig_dev_read)  # noqa: B010
+    setattr(_DAttr, "_write", _orig_dev_write)  # noqa: B010
+    # setattr(_CtxAttr, "_read", _orig_ctx_read)  # noqa: B010
+    # setattr(_CtxAttr, "_write", _orig_ctx_write)  # noqa: B010
 
 
 logger.debug("iio.py monkey patch applied")
